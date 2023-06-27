6678aa7bc050647753eef1755f89f9c9cc558b34
Armin Roșu <git@armin.ro>
2023-06-27 11:58:09 +0200

    Add prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// @see http://json.schemastore.org/prettierrc
+
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  plugins: [],
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+}
